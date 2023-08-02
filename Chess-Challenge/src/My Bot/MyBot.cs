67e869ca4071ef53e1dc3fa8d54a25c90d60d601
Nikita-Babich <nikita.babich.a@gmail.com>
2023-08-02 14:29:41 +0300

    Update MyBot.cs

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -2,22 +2,78 @@
 using System;
 using System.Collections.Generic;
 
+// first time ever i am programming in c#, and first time - bots
+
 public class MyBot : IChessBot
 {
-	//I tried to implement the same way of thinking that i use when playing chess against people.
-    //Note: checkmate and stalemate - both can be simply defined as a situation with 0 legal moves.
-    //The algorithm tries to achieve any of those by reducing opponent's freedom.
-	//private float material_dominance = 1;
+	private bool i_am_white;
+	private int army_weight = 1;
+	private int freedom_weight = 1;
+	private int material_weight = 1;
+	
 	private Dictionary<Move, int> moveValueDictionary = new Dictionary<Move, int>();
+	
+	//private int Spread = 5;
+	//private int Depth = 5;
+	//private int Recent_Evaluation = 0;
+	
+	
+	private int Evaluate_army(Board board)
+	{
+		int army_size_dom = 0;
+		army_size_dom += BitboardHelper.GetNumberOfSetBits(board.WhitePiecesBitboard);
+		army_size_dom -= BitboardHelper.GetNumberOfSetBits(board.BlackPiecesBitboard);
+		return army_size_dom;
+	}
+	
+	
+	private int Evaluate_material(Board board)
+	{
+		int army_size_dom = 0;
+		//BitboardHelper.GetNumberOfSetBits(board.WhitePiecesBitboard) - 
+		//BitboardHelper.GetNumberOfSetBits(board.BlackPiecesBitboard);
+		return army_size_dom;
+	}
+	
+	
+	private int Evaluate_freedom(Board board)
+	{
+		int white_move_mult = (board.IsWhiteToMove ? 1 : -1);
+		int freedom_current = board.GetLegalMoves().Length;
+
+		board.ForceSkipTurn();
+		int freedom_opponent = board.GetLegalMoves().Length;
+		board.UndoSkipTurn();
+		
+		return white_move_mult * (freedom_current - freedom_opponent);
+	}
+	
+	
+	private int Evaluate_position(Board board) //higher better for white
+	{
+		int result = 0;
+		result += freedom_weight * Evaluate_freedom(board);
+		result += army_weight * Evaluate_army(board);
+		//result += army_weight * Evaluate_material(board);
+		return result; 
+	}
+
+
+	
+	
 	
     public Move Think(Board board, Timer timer)
 	{
 		moveValueDictionary.Clear();
 		bool i_am_white = board.IsWhiteToMove;
-		ulong attacked_squares = 0;
-		ulong defended_squares = 0;
 		
-		
+		//ulong attacked_squares = 0;
+		//ulong defended_squares = 0;
+		//int current_material = 0;
+		//int current_enemy_material = 0;
+		//int current_difference = 0;
+		//int expected_difference = 0;
+
 		//GetNumberOfSetBits(WhitePiecesBitboard)
 		//GetPieceBitboard(PieceType type, bool white)
 		
@@ -28,23 +84,31 @@ public class MyBot : IChessBot
         Move[] allMoves = board.GetLegalMoves();
         foreach (Move move in allMoves)
 		{
-			board.MakeMove(move);
-			
-			if (board.IsInCheckmate()) return move;
-			moveValueDictionary.Add(
-				move, board.GetLegalMoves().Length;
-				
+			board.MakeMove(move);		
+			if (board.IsInCheckmate()) return move;			
+			moveValueDictionary.Add(move, Evaluate_position(board));
 			board.UndoMove(move);
         }
 		
 		Move final_move = allMoves[0];
-		int least_freedom = 300;
+		int best_score = i_am_white ? -1000 : 1000;
 		foreach (var pair in moveValueDictionary)
 		{
-			if(pair.Value < least_freedom)
+			if(i_am_white)
+			{	
+				if(pair.Value > best_score)
+				{
+					final_move = pair.Key;
+					best_score = pair.Value;
+				}
+			}
+			else
 			{
-				final_move = pair.Key;
-				least_freedom = pair.Value;
+				if(pair.Value < best_score)
+				{
+					final_move = pair.Key;
+					best_score = pair.Value;
+				}
 			}
 		}
 		
